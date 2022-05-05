a8a49c648e9fc6f4c274579fdab8578f533395ef
Guido Sabik <guido.sabik@gmail.com>
2022-05-05 19:34:52 -0300

    add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,21 @@
+import { Flex, Text } from "@chakra-ui/react"
+
+const Footer = () => {
+    return (
+        <Flex 
+            alignItems="center"
+            bgColor="gray.50"
+            h="5rem"
+            justifyContent="center" 
+            w="full" 
+        >
+            <Text
+                fontSize="lg" 
+                fontWeight="500"
+            >
+                Developed by Guido Sabik</Text>
+        </Flex>
+    )
+}
+
+export default Footer
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,17 @@
 import React from "react"
-import { Flex, Heading } from "@chakra-ui/react"
+import { Flex, Heading, Text } from "@chakra-ui/react"
 
 const Header = () => {
     return (
-        <Flex 
-            justifyContent="center" 
+        <Flex
+            alignItems="center"
+            bgColor="gray.50"
+            justifyContent="space-between" 
             p={6}
             w="full" 
         >
             <Heading>Giphy API</Heading>
+            <Text fontSize="lg">Favorites</Text>
         </Flex>
     )
 }
